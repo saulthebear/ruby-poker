2996775d256ec545b17bbb3471dd8dfa6e00ec6e
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-29 16:50:32 -0600

    Add hand rankings to constants

--- lib/card_constants.rb
+++ lib/card_constants.rb
@@ -1,9 +1,23 @@
 module CardConstants
   RANKS = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'].freeze
+
   SUITS = [
     Suit.new('spade'),
     Suit.new('diamond'),
     Suit.new('club'),
     Suit.new('heart')
+  ].freeze
+
+  HAND_RANKINGS = %i[
+    high_card
+    pair
+    two_pair
+    three_of_a_kind
+    straight
+    flush
+    full_house
+    four_of_a_kind
+    straight_flush
+    royal_flush
   ].freeze
 end
