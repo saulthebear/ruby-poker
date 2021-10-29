7039288505eb889bb0e37a77c9e08cdf7d047f42
Stefan Vosloo <saulthebear@users.noreply.github.com>
2021-10-29 16:50:52 -0600

    Implement hand type comparisons

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,6 +1,9 @@
 require_relative 'card'
+require_relative 'card_constants'
 
 class Hand
+  include CardConstants
+
   attr_reader :cards
 
   def initialize
@@ -19,5 +22,166 @@ class Hand
     raise ArgumentError, 'Not a card.' unless new_card.is_a?(Card)
 
     @cards[index] = new_card
+  end
+
+  def type
+    return :royal_flush if royal_flush?
+
+    return :straight_flush if straight_flush?
+
+    return :four_of_a_kind if four_of_a_kind?
+
+    return :full_house if full_house?
+
+    return :flush if flush?
+
+    return :straight if straight?(rank_indices) || straight?(rank_indices(ace_high: true))
+
+    return :three_of_a_kind if three_of_a_kind?
+
+    return :two_pair if two_pair?
+
+    return :pair if pair?
+
+    :high_card
+  end
+
+  def highest_card
+    card_values = rank_indices(ace_high: true)
+    max_card_value = card_values.max
+    index_of_max_card = card_values.index(max_card_value)
+    @cards[index_of_max_card]
+  end
+
+  def <=>(other)
+    my_type = type
+    other_type = other.type
+    my_ranking = HAND_RANKINGS.index(my_type)
+    other_ranking = HAND_RANKINGS.index(other_type)
+
+    if my_type == :high_card && other_type == :high_card
+      my_ranking = rank_index(highest_card, ace_high: true)
+      other_ranking = rank_index(other.highest_card, ace_high: true)
+    end
+
+    my_ranking <=> other_ranking
+  end
+
+  private
+
+  def royal_flush?
+    return false unless flush?
+
+    required_ranks = %w[A K Q J 10]
+    ranks.map(&:to_s).sort == required_ranks.map(&:to_s).sort
+  end
+
+  def straight_flush?
+    return false unless all_same_suit?
+
+    straight?(rank_indices) || straight?(rank_indices(ace_high: true))
+  end
+
+  def straight?(rank_indices)
+    sorted_rank_indices = rank_indices.sort
+    (0...4).each do |index|
+      rank1 = sorted_rank_indices[index]
+      rank2 = sorted_rank_indices[index + 1]
+      return false unless (rank2 - rank1) == 1
+    end
+    true
+  end
+
+  def four_of_a_kind?
+    n_of_a_kind?(4).length.positive?
+  end
+
+  def full_house?
+    three_of_a_kind? && pair?
+  end
+
+  def flush?
+    all_same_suit?
+  end
+
+  def three_of_a_kind?
+    n_of_a_kind?(3).length.positive?
+  end
+
+  def two_pair?
+    n_of_a_kind?(2).length == 2
+  end
+
+  def pair?
+    n_of_a_kind?(2).length.positive?
+  end
+
+  def rank_counts
+    rank_counts = Hash.new(0)
+    RANKS.each do |rank|
+      rank_counts[rank] += ranks.count { |card_rank| card_rank == rank }
+    end
+    rank_counts
+  end
+
+  def n_of_a_kind?(n)
+    matches = []
+    rank_counts.each do |rank, count|
+      matches << rank if count == n
+    end
+    matches
+  end
+
+  def rank_indices(ace_high: false)
+    if ace_high
+      rank_indices_ace_high
+    else
+      rank_indices_ace_low
+    end
+  end
+
+  def rank_indices_ace_low
+    @cards.map { |card| rank_index(card) }
+  end
+
+  def rank_indices_ace_high
+    @cards.map do |card|
+      if card.rank == 'A'
+        high_ace_rank(card)
+      else
+        rank_index(card)
+      end
+    end
+  end
+
+  def all_same_suit?
+    suits.uniq.length == 1
+  end
+
+  def suits
+    @cards.map(&:suit)
+  end
+
+  def ranks
+    @cards.map(&:rank)
+  end
+
+  def rank_index(card, ace_high: false)
+    return 13 if ace_high && card.rank == 'A'
+
+    RANKS.index(card.rank)
+  end
+
+  def high_ace_rank(card)
+    raise ArgumentError, 'Not an Ace' unless card.rank == 'A'
+
+    high_ace = {
+      'spade' => 13,
+      'diamond' => 26,
+      'club' => 39,
+      'heart' => 52
+    }
+
+    high_ace[card.suit.name]
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -3,6 +3,11 @@ require 'hand'
 
 describe Hand do
   subject = Hand.new
+  let(:spade) { Suit.new('spade') }
+  let(:club) { Suit.new('club') }
+  let(:heart) { Suit.new('heart') }
+  let(:diamond) { Suit.new('diamond') }
+
   describe '#initialize' do
     it 'starts with no cards' do
       expect(subject.cards).to be_empty
@@ -83,21 +88,351 @@ describe Hand do
   end
 
   describe '#type' do
-    it 'identifies Royal Flush'
-    it 'identifies Straight Flush'
-    it 'identifies Four of a Kind'
-    it 'identifies Full House'
-    it 'identifies Flush'
-    it 'identifies Straight'
-    it 'identifies Three of a Kind'
-    it 'identifies Two Pair'
-    it 'identifies Pair'
-    it 'identifies High Card'
+    it 'identifies Royal Flush' do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(spade, 'K'))
+      hand.add_card(Card.new(spade, 'Q'))
+      hand.add_card(Card.new(spade, 'J'))
+      hand.add_card(Card.new(spade, 10))
+      expect(hand.type).to be(:royal_flush)
+    end
+
+    context 'Straight flush' do
+      it 'identified correctly' do
+        hand = Hand.new
+        hand.add_card(Card.new(spade, 8))
+        hand.add_card(Card.new(spade, 5))
+        hand.add_card(Card.new(spade, 7))
+        hand.add_card(Card.new(spade, 6))
+        hand.add_card(Card.new(spade, 4))
+        expect(hand.type).to be(:straight_flush)
+      end
+
+      it 'with Ace low identified correclty' do
+        hand = Hand.new
+        hand.add_card(Card.new(spade, 'A'))
+        hand.add_card(Card.new(spade, 2))
+        hand.add_card(Card.new(spade, 3))
+        hand.add_card(Card.new(spade, 4))
+        hand.add_card(Card.new(spade, 5))
+        expect(hand.type).to be(:straight_flush)
+      end
+    end
+
+    context 'Four of a Kind' do
+      it 'identified correclty - 4 Aces' do
+        hand = Hand.new
+        hand.add_card(Card.new(club, 3))
+        hand.add_card(Card.new(spade, 'A'))
+        hand.add_card(Card.new(diamond, 'A'))
+        hand.add_card(Card.new(heart, 'A'))
+        hand.add_card(Card.new(club, 'A'))
+        expect(hand.type).to be(:four_of_a_kind)
+      end
+
+      it 'identified correclty - 4 Kings' do
+        hand = Hand.new
+        hand.add_card(Card.new(club, 3))
+        hand.add_card(Card.new(spade, 'K'))
+        hand.add_card(Card.new(diamond, 'K'))
+        hand.add_card(Card.new(heart, 'K'))
+        hand.add_card(Card.new(club, 'K'))
+        expect(hand.type).to be(:four_of_a_kind)
+      end
+
+      it 'identified correclty - 4 Fours' do
+        hand = Hand.new
+        hand.add_card(Card.new(club, 3))
+        hand.add_card(Card.new(spade, 4))
+        hand.add_card(Card.new(diamond, 4))
+        hand.add_card(Card.new(heart, 4))
+        hand.add_card(Card.new(club, 4))
+        expect(hand.type).to be(:four_of_a_kind)
+      end
+    end
+
+    it 'identifies Full House' do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 10))
+      hand.add_card(Card.new(heart, 10))
+      hand.add_card(Card.new(club, 10))
+      hand.add_card(Card.new(spade, 9))
+      hand.add_card(Card.new(diamond, 9))
+      expect(hand.type).to be(:full_house)
+    end
+
+    it 'identifies Flush' do
+      hand = Hand.new
+      hand.add_card(Card.new(heart, 4))
+      hand.add_card(Card.new(heart, 'J'))
+      hand.add_card(Card.new(heart, 8))
+      hand.add_card(Card.new(heart, 2))
+      hand.add_card(Card.new(heart, 9))
+      expect(hand.type).to be(:flush)
+    end
+
+    it 'identifies Straight' do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 6))
+      hand.add_card(Card.new(spade, 2))
+      hand.add_card(Card.new(spade, 3))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(club, 5))
+      expect(hand.type).to be(:straight)
+    end
+
+    it 'identifies Straight with Ace low' do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(spade, 2))
+      hand.add_card(Card.new(spade, 3))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(club, 5))
+      expect(hand.type).to be(:straight)
+    end
+
+    it 'identifies Straight with Ace high' do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(club, 'K'))
+      hand.add_card(Card.new(spade, 'Q'))
+      hand.add_card(Card.new(spade, 'J'))
+      hand.add_card(Card.new(spade, 10))
+      expect(hand.type).to be(:straight)
+    end
+
+    it 'identifies Three of a Kind' do
+      hand = Hand.new
+      hand.add_card(Card.new(club, 7))
+      hand.add_card(Card.new(diamond, 7))
+      hand.add_card(Card.new(spade, 7))
+      hand.add_card(Card.new(club, 'K'))
+      hand.add_card(Card.new(diamond, 3))
+      expect(hand.type).to be(:three_of_a_kind)
+    end
+
+    it 'identifies Two Pair' do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(club, 4))
+      hand.add_card(Card.new(club, 3))
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'Q'))
+      expect(hand.type).to be(:two_pair)
+    end
+
+    it 'identifies Pair' do
+      hand = Hand.new
+      hand.add_card(Card.new(heart, 'A'))
+      hand.add_card(Card.new(diamond, 'A'))
+      hand.add_card(Card.new(club, 8))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(heart, 7))
+      expect(hand.type).to be(:pair)
+    end
+
+    it 'identifies High Card' do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'J'))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(heart, 4))
+      hand.add_card(Card.new(spade, 2))
+      expect(hand.type).to be(:high_card)
+    end
+  end
+
+  describe '#highest_card' do
+    it 'identifies highest number card' do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 5))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 2))
+      hand.add_card(Card.new(spade, 4))
+      expect(hand.highest_card).to eq(Card.new(spade, 8))
+    end
+
+    it 'identifies face card higher than number card' do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'J'))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(spade, 2))
+      expect(hand.highest_card).to eq(Card.new(club, 'J'))
+    end
+
+    it 'identifies highest face card' do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'J'))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 'K'))
+      hand.add_card(Card.new(spade, 2))
+      expect(hand.highest_card).to eq(Card.new(spade, 'K'))
+    end
+
+    it 'identifies High Card with Ace' do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'J'))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(spade, 2))
+      expect(hand.highest_card).to eq(Card.new(spade, 'A'))
+    end
   end
 
   describe '#<=>(other_hand)' do
-    it 'returns -1 if less than other_hand'
-    it 'returns 0 if equal to other_hand'
-    it 'returns 1 if greater than to other_hand'
+    let(:royal_flush) do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(spade, 'K'))
+      hand.add_card(Card.new(spade, 'Q'))
+      hand.add_card(Card.new(spade, 'J'))
+      hand.add_card(Card.new(spade, 10))
+      hand
+    end
+
+    let(:straight_flush) do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 5))
+      hand.add_card(Card.new(spade, 7))
+      hand.add_card(Card.new(spade, 6))
+      hand.add_card(Card.new(spade, 4))
+      hand
+    end
+
+    let(:four_of_a_kind) do
+      hand = Hand.new
+      hand.add_card(Card.new(club, 3))
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(diamond, 'A'))
+      hand.add_card(Card.new(heart, 'A'))
+      hand.add_card(Card.new(club, 'A'))
+      hand
+    end
+
+    let(:full_house) do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 10))
+      hand.add_card(Card.new(heart, 10))
+      hand.add_card(Card.new(club, 10))
+      hand.add_card(Card.new(spade, 9))
+      hand.add_card(Card.new(diamond, 9))
+      hand
+    end
+
+    let(:flush) do
+      hand = Hand.new
+      hand.add_card(Card.new(heart, 4))
+      hand.add_card(Card.new(heart, 'J'))
+      hand.add_card(Card.new(heart, 8))
+      hand.add_card(Card.new(heart, 2))
+      hand.add_card(Card.new(heart, 9))
+      hand
+    end
+
+    let(:straight) do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 6))
+      hand.add_card(Card.new(spade, 2))
+      hand.add_card(Card.new(spade, 3))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(club, 5))
+      hand
+    end
+
+    let(:three_of_a_kind) do
+      hand = Hand.new
+      hand.add_card(Card.new(club, 7))
+      hand.add_card(Card.new(diamond, 7))
+      hand.add_card(Card.new(spade, 7))
+      hand.add_card(Card.new(club, 'K'))
+      hand.add_card(Card.new(diamond, 3))
+      hand
+    end
+
+    let(:two_pair) do
+      hand = Hand.new
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(club, 4))
+      hand.add_card(Card.new(club, 3))
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'Q'))
+      hand
+    end
+
+    let(:pair) do
+      hand = Hand.new
+      hand.add_card(Card.new(heart, 'A'))
+      hand.add_card(Card.new(diamond, 'A'))
+      hand.add_card(Card.new(club, 8))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(heart, 7))
+      hand
+    end
+
+    let(:eight_high) do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 5))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 2))
+      hand.add_card(Card.new(spade, 4))
+      hand
+    end
+
+    let(:jack_high) do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'J'))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 4))
+      hand.add_card(Card.new(spade, 2))
+      hand
+    end
+
+    let(:ace_high) do
+      hand = Hand.new
+      hand.add_card(Card.new(diamond, 3))
+      hand.add_card(Card.new(club, 'J'))
+      hand.add_card(Card.new(spade, 8))
+      hand.add_card(Card.new(spade, 'A'))
+      hand.add_card(Card.new(spade, 2))
+      hand
+    end
+
+    it 'returns -1 if less than other_hand' do
+      expect(ace_high <=> royal_flush).to eq(-1)
+      expect(straight_flush <=> royal_flush).to eq(-1)
+      expect(four_of_a_kind <=> straight_flush).to eq(-1)
+    end
+
+    it 'returns 0 if equal to other_hand' do
+      hand = Hand.new
+      hand.add_card(Card.new(club, 3))
+      hand.add_card(Card.new(spade, 'K'))
+      hand.add_card(Card.new(diamond, 'K'))
+      hand.add_card(Card.new(heart, 'K'))
+      hand.add_card(Card.new(club, 'K'))
+
+      expect(hand <=> four_of_a_kind).to eq(0)
+    end
+
+    it 'returns 1 if greater than to other_hand' do
+      expect(royal_flush <=> ace_high).to eq(1)
+      expect(straight_flush <=> four_of_a_kind).to eq(1)
+      expect(two_pair <=> pair).to eq(1)
+    end
+
+    it 'correctly compares high cards' do
+      expect(eight_high <=> jack_high).to eq(-1)
+      expect(jack_high <=> ace_high).to eq(-1)
+      expect(ace_high <=> eight_high).to eq(1)
+    end
   end
 end
